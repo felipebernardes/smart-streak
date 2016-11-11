9f8146370e549806b558db3becadeb073b46f925
Natasha <natasha.matias@tegra.me>
2016-11-11 12:53:58 -0200

    Join htmls

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var app = angular.module('myApp', []);
 
 app.controller('AppCtrl',['$scope', function($scope, $location, $http){
-  $scope.test = 'Hello World';
 
   $scope.days = [];
   for (i = 0; i < 364; i++) {
