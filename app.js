3852f28db6226c702ac7f02e188142274a691890
Felipe Bernardes <bernardes.flp@gmail.com>
2016-11-11 11:00:06 -0200

    Itarates html structure

--- app.js
+++ app.js
@@ -2,5 +2,12 @@ var app = angular.module('myApp', []);
 
 app.controller('AppCtrl',['$scope', function($scope, $location, $http){
   $scope.test = 'Hello World';
-  
+
+  $scope.days = [];
+  for (i = 0; i < 364; i++) {
+    $scope.days.push({
+          status: true
+    });
+  }
+
 }]);
